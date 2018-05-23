00e377729aabcdb96bc10ec2f7ffccea6547c4cc
Антон Синицын <antony@email.su>
2018-05-23 15:41:23 +0300

    Кажется, теперь можем создавать снапшоты

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
+extern crate chrono;
+
 fn main() {
-    // zfs::snapshots::new("rpool/ROOT/ubuntu", "version2");
-    let _ = zfs::snapshots::list();
-    //zfs::snapshots::remove("rpool/ROOT/ubuntu@version2");
+    zfs::snapshots::new("rpool/ROOT/ubuntu");
+    let list = zfs::snapshots::list();
+    println!("{:?}", list);
+    zfs::snapshots::remove("rpool/ROOT/ubuntu@version2");
 }
 
 pub mod zfs {
     pub mod snapshots {
         use std::process::Command;
+        use chrono::prelude::*; // https://github.com/chronotope/chrono
 
-        pub fn new(pool_name: &str, snap_name: &str) {
-            Command::new("zfs")
+        pub fn new(pool_name: &str) {
+            let dt = Local::now();
+            let snap_name = format!("{}_{}_{}__{}_{}", dt.day(), dt.month(), dt.year(), dt.hour(), dt.minute());
+            let buffer = Command::new("zfs")
                 .args(&["snapshot", &format!("{}@{}", pool_name, snap_name)])
                 .output()
-                .expect("failed to execute process");
+                .expect("Не смог создать снапшот");
         }
 
-        pub fn list() -> Option<Vec<String>> {
+        pub fn list() -> Vec<String> {
             /*
             let output = Command::new("zfs")
                 .args(&["list", "-t", "snapshots"])
                 .output()
-                .expect("failed to execute process");
+                .expect("Не могу получить список снапшотов");
             */
 
             let output = "NAME                         USED  AVAIL  REFER  MOUNTPOINT
@@ -28,26 +34,20 @@ pub mod zfs {
                           rpool/ROOT/ubuntu@version1  6,20M      -   643M  -
                           rpool/ROOT/ubuntu@gitlab1    211M      -   898M  -"
                 .to_owned();
-            let mut list: Vec<String> = Vec::new();
-            for line in output.lines() {
-                let name = line.split_whitespace().next();
-                if let Some(snap_name) = name {
-                    println!("{}", snap_name);
-                    list.push(snap_name.to_owned());
-                }
-            }
-            Some(
-                list.into_iter()
-                    .filter(|x| !(x == "NAME"))
-                    .collect::<Vec<String>>(),
-            )
+
+            output
+                .lines()
+                .filter_map(|line| line.split_whitespace().next())
+                .filter(|s| s != &"NAME")
+                .map(|s| s.to_owned())
+                .collect()
         }
 
         pub fn remove(full_snap_name: &str) {
             Command::new("zfs")
                 .args(&["destroy", full_snap_name])
                 .output()
-                .expect("failed to execute process");
+                .expect("Не могу уничтожить снапшот");
         }
     }
 }
